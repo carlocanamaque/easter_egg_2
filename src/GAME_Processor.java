a6a9f1ca24b6273536ca10f2d17a7eabdf22e22c
carlocanamaque <carlocanamaque@proton.me>
2025-07-21 06:10:56 +0800

    Discarded GAME_Launcher.java and instead implemented JFrame on GAME_Processor.java

--- src/GAME_Processor.java
+++ src/GAME_Processor.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyListener;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
@@ -18,7 +19,7 @@ public class GAME_Processor extends JPanel implements Runnable {
 	final int MAX_SCREEN_COL = 16;
 	final int MAX_SCREEN_ROW = 12;
 	final int SCREEN_WIDTH = (TILE_SIZE * MAX_SCREEN_COL);
-	final int SCREEN_HEIGHT = (TILE_SIZE * MAX_SCREEN_COL);
+	final int SCREEN_HEIGHT = (TILE_SIZE * MAX_SCREEN_ROW);
 
 	final int FRAME_RATE_PER_SEC = 60;
 
@@ -78,6 +79,23 @@ public class GAME_Processor extends JPanel implements Runnable {
 				delta--;
 			}
 		}
+	}
+
+	public static void main(String[] args) {
+
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("easter_egg_1");
+
+		GAME_Processor processor = new GAME_Processor();
+		window.add(processor);
+		window.pack();
+
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+
+		processor.startThread();
 	}
 }
 
