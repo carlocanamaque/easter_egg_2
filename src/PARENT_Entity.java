645689b44b730187fb1bfcf0c54556e22dce8235
carlocanamaque <carlocanamaque@proton.me>
2025-07-21 07:21:46 +0800

    Declared fundamental parent class variables

--- src/PARENT_Entity.java
+++ src/PARENT_Entity.java
@@ -1,1 +1,13 @@
 package src;
+
+import java.awt.Image;
+
+
+public class PARENT_Entity {
+
+	int x, y, speed;
+
+	String direction;
+	
+	Image up, down, left, right, upleft, upright, downleft, downright;
+}
