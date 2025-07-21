5a40253469522b330ae0f7ccd3dcd7ebe194236d
carlocanamaque <carlocanamaque@proton.me>
2025-07-21 09:42:35 +0800

    Fixed faulty logic in randomized tile selection section

--- src/GAME_Map.java
+++ src/GAME_Map.java
@@ -11,6 +11,8 @@ public class GAME_Map{
 	GAME_Processor processor;
 	PARENT_Tile[] tiles = new PARENT_Tile[3];
 
+	int[] randInts;
+
 	public GAME_Map(GAME_Processor processor) {
 
 		this.processor = processor;
@@ -21,23 +23,30 @@ public class GAME_Map{
 		tiles[1].gif = Toolkit.getDefaultToolkit().createImage("res/TILE_Grass_2.gif");
 		tiles[2] = new PARENT_Tile();
 		tiles[2].gif = Toolkit.getDefaultToolkit().createImage("res/TILE_Grass_3.gif");
-	}
 
+		int limit = (processor.MAX_SCREEN_ROW * processor.MAX_SCREEN_COL);
+		randInts = new int[limit];
+		for(int i = 0; i < limit; i++) {
+
+			double rand = (Math.random() * 3);
+			int randInt = ((int) rand);
+			randInts[i] = randInt;
+		}
+	}
 	public void draw(Graphics2D g2) {
 
-		double rand;
 		int col = 0;
 		int row = 0;
 		int x = 0;
 		int y = 0;
+		int j = 0;
 
 		while(col < processor.MAX_SCREEN_COL && row < processor.MAX_SCREEN_ROW) {
 
-			rand = (Math.random() * 3);
-			int randInt = ((int) rand);
-			g2.drawImage(tiles[randInt].gif, x, y, processor.TILE_SIZE, processor.TILE_SIZE, processor);
+			g2.drawImage(tiles[randInts[j]].gif, x, y, processor.TILE_SIZE, processor.TILE_SIZE, processor);
 			x += processor.TILE_SIZE;
 			col++;
+			j++;
 
 			if(col == processor.MAX_SCREEN_COL) {
 
